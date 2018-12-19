b55ec921c67af2be02c90d87ba07d82773aed3fd
digitalsoba <anthony.inlavong@gmail.com>
2018-12-18 17:23:12 -0800

    Add redhat/debian based roles

---
+++ terraform/spot-instance/main.tf
@@ -0,0 +1,28 @@
+variable "aws_region" {
+  default = "us-west-2"
+}
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "matabit-terraform-backend"
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock-dynamo"
+    region         = "us-west-2"
+    key            = "spot-instance/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config {
+    bucket = "matabit-terraform-backend"
+    region = "us-west-2"
+    key    = "vpc/terraform.tfstate"
+  }
+}
+
