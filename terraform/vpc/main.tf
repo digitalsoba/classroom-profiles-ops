62e5827e0bb46e82381cad3155845d38e9afe0ce
digitalsoba <anthony.inlavong@gmail.com>
2019-01-03 09:31:11 -0800

    Add logstash to NAT SG

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -132,6 +132,13 @@ resource "aws_security_group" "nat" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 5044
+    to_port     = 5044
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   vpc_id = "${aws_vpc.matabit_vpc.id}"
 
   tags {
