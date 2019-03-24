35a8fa119717fe5397b70a06eafc7eff04a90c1d
digitalsoba <anthony.inlavong@gmail.com>
2019-03-24 09:26:31 -0700

    Kube

--- terraform/spot-instance/main.tf
+++ terraform/spot-instance/main.tf
@@ -94,6 +94,7 @@ resource "aws_spot_instance_request" "classroom-profiles" {
 #   }
 # }
 
+
 # resource "aws_spot_instance_request" "apollo" {
 #   ami                         = "${var.ami_id}"
 #   spot_price                  = "0.0040"
@@ -118,27 +119,3 @@ resource "aws_spot_instance_request" "classroom-profiles" {
 #   }
 # }
 
-
-# resource "aws_spot_instance_request" "kube" {
-#   ami                         = "${var.ami_id}"
-#   spot_price                  = "0.0035"
-#   instance_type               = "t2.micro"
-#   key_name                    = "${var.key_name}"
-#   monitoring                  = true
-#   associate_public_ip_address = true
-#   count                       = 3
-#   wait_for_fulfillment        = true
-#   user_data                   = "${file("../cloud-init.conf")}"
-#   security_groups             = ["${module.web_server_sg.this_security_group_id}"]
-#   subnet_id                   = "${data.terraform_remote_state.vpc.public_subnet_a}"
-
-
-#   provisioner "local-exec" {
-#     command = "aws ec2 create-tags --resources ${self.spot_instance_id} --tags Key=Name,Value=kube-${count.index}"
-#   }
-
-
-#   tags {
-#     Name = "kube"
-#   }
-# }
--- terraform/spot-instance/variables.tf
+++ terraform/spot-instance/variables.tf
@@ -23,6 +23,7 @@ variable "ami_id" {
   description = "AMI ID for spot instance (ubuntu 18.04 us-west-2)"
   default     = "ami-0bbe6b35405ecebdb"
 }
+
 variable "key_name" {
   default = "anthony"
 }
